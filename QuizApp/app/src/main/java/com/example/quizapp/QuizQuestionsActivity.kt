fd2907100d3f8362bf5e3c90f0ea411ea463f564
William Nkosi <williamnkosi09@gmail.com>
2020-08-03 15:15:58 -0500

    QuizApp | Implemented the QuizQuestions UI sections,  configured the QuizQuestionsActivity functionality to present data from objects

--- QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,18 @@ class QuizQuestionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz_questions)
 
         val questionList = Constants.getQuestions()
-      
+
         Log.i("Questions Size", "${questionList.size}")
+        val currentQuestion =  1
+        val question: Question? = questionList[currentQuestion -1]
+        progressBar.progress = currentQuestion
+        tv_progress.text = "$currentQuestion/${progressBar.max}"
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question!!.optionOne
+        tv_option_two.text = question!!.optionTwo
+        tv_option_three.text = question!!.optionThree
+        tv_option_four.text = question!!.optionFour
+        val correctAnswer = questionList[0].correctAnswer
     }
 }
