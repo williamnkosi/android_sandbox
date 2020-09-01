086c83599d0fc0b2a6fc189c95f6a4b34032d70e
William Nkosi <william.dev.acc@gmail.com>
2020-09-01 13:02:51 -0500

    MinuteWorkout | Init setup of the project

---
+++ MinuteWorkout/app/src/main/java/com/example/minuteworkout/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.minuteworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ll_start.setOnClickListener{
+            Toast.makeText(this, "Here we will start the exercise",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+}
